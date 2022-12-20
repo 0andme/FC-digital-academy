577bb6e845ce5c4a76be6784143eabed1cb67bfc
0andme <yu00mi97@gmail.com>
2022-12-20 23:10:50 +0900

    feat : 방명록 페이지네이션 기능 추가

--- mobile-invitations/app.py
+++ mobile-invitations/app.py
@@ -1,3 +1,4 @@
+import math
 import datetime
 from flask import Flask,render_template,jsonify,request,redirect
 from flask_pymongo import PyMongo
@@ -20,15 +21,26 @@ def write():
 
 @app.route('/')
 def index():
-  # 입력된 날짜와 현재 시간과의 
+  # 입력된 날짜와 현재 시간과의 차이 변수에 저장
   now =datetime.datetime.now()
   wedding=datetime.datetime(2022,12,19,0,0,0)
   diff=(wedding-now).days 
   
-  # db 데이터 가져오기
-  guestbooks=mongo.db['wedding'].find()
+  # db 데이터 가져오기 - limit & skip
+  page= int(request.args.get('page',1) )
+  limit=3
+  skip=(page-1)*limit
   
-  return render_template('index.html',diff=diff,guestbooks=guestbooks)
+  guestbooks=mongo.db['wedding'].find().limit(limit).skip(skip)
+  
+  # count_documents 문서에 대한 개수 찾기
+  # 파라미터로 값을 넣지 않아도 되지만, 
+  # 모든 문서의 개수를 알고자 한다면 빈 값{}을 넣어줘야함
+  count=mongo.db['wedding'].count_documents({})
+  max_page=math.ceil(count/limit)
+  pages=range(1,max_page+1)
+
+  return render_template('index.html',diff=diff,guestbooks=guestbooks,pages=pages)
 
 if __name__ == '__main__':
   app.run()
